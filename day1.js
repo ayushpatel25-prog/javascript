98991b3e27f3189afc505825b199e09a01f198a6
ayush patel <patelayushsbr2510@gmail.com>
2026-07-28 01:27:44 +0530

    da1

---
+++ day1.js
@@ -0,0 +1,15 @@
+// operations
+var a=20;
+var b=10;
+var sum=a+b;
+var sub=a-b;
+var mul=a*b;
+var div=a/b;
+var mod=a%b;
+var exp=a**b;
+console.log("Sum: "+sum);
+console.log("Sub: "+sub);
+console.log("Mul: "+mul);
+console.log("Div: "+div);
+console.log("Mod: "+mod);
+console.log("Exp: "+exp);
