2e532335599611008e717c8a4655e6422dd8c519
ayush patel <patelayushsbr2510@gmail.com>
2026-07-31 01:55:52 +0530

    day32

--- jspractice/day3.js
+++ jspractice/day3.js
@@ -12,4 +12,29 @@ console.log(c.toLowerCase());
 
 // indexOf
 let str="ILoveCoding";
-console.log(str.indexOf("Love"));
+// console.log(str.indexOf("Love"));
+
+// method chaining
+
+let str1="   ILoveCoding   ";
+let ms=str1.trim().toUpperCase();
+console.log(ms);
+
+// slice
+console.log(str.slice(1,3));
+
+// substring
+
+// replace
+
+console.log(str.replace("love","do"));
+
+// repeat
+console.log(str.repeat(5));
+
+// array 
+
+let student =["ayush",22,5.8];
+console.log(student[2]);
+console.log(student[0][1]);
+
